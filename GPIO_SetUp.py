86e7e8741395f94dc40c3e9a1b8dfe4222b4a6d1
amirbeek <Amirbek.shomurodov01@gmail.com>
2025-03-18 21:39:03 +0000

    set up camera

--- GPIO_SetUp.py
+++ GPIO_SetUp.py
@@ -1,32 +1,49 @@
 from gpiozero import MotionSensor, LED
+from picamzero import Camera
 from signal import pause
 import time
+import os
 
 # Global Variables
 time_motion_started = time.time()
+last_time_photo_taken = 0
 
 # Set up GPIO
 pir = MotionSensor(4)
 led = LED(17)
+MOVEMENT_DETECTED_TRESHOLD = 5.0
+MIN_DURATION_BETWEEN_PHOTOS = 30.0
+CAMERA_FOLDER_PATH = "./src/images_fl"
+if not os.path.exists(CAMERA_FOLDER_PATH):
+    os.mkdir(CAMERA_FOLDER_PATH)
 print("GPIOs setup Done!")
+
+# Set up Camera
+camera = Camera()
+camera.still_size(1536, 864)
+camera.flip_camera(vflip=True, hflip=True)
+time.sleep(2)
+print("Camera set up Okay!")
 
 
 def motion_detected():
-    print("Starting timer")
     global time_motion_started
+    print("Starting timer")
     time_motion_started = time.time()
     led.on()
 
 
 def motion_finished():
+    global last_time_photo_taken
     led.off()
     motion_duration = time.time() - time_motion_started
     print(motion_duration)
-    if motion_duration > 5.0:
-        print('Taking a photo and selling it by email')
+    if motion_duration > MOVEMENT_DETECTED_TRESHOLD:
+        if time.time() - last_time_photo_taken > MIN_DURATION_BETWEEN_PHOTOS:
+            last_time_photo_taken = time.time()
 
 
-pir.when_motion = motion_detected()
-pir.when_no_motion = motion_finished()
+pir.when_motion = motion_detected
+pir.when_no_motion = motion_finished
 
 pause()
